cfea0f4a69773238c386637614471d45151835ba
Gento <a551.return@gmail.com>
2018-04-11 14:08:53 +0900

    オフラインでもキャッシュで高速に動く

--- work/scripts/app.js
+++ work/scripts/app.js
@@ -363,4 +363,13 @@ app.saveSelectedCities = function() {
 
 
   // TODO add service worker code here
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
+
+
+
+  
 })();
